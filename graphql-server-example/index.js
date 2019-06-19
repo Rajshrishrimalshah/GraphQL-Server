b34d1ac679696945e64d5038c7a79842549c24c3
Rajshrishrimalshah <raj.shrishrimalshah@successive.tech>
2019-06-19 19:52:18 +0530

    feat: done type relation in GraphQL'

--- graphql-server-example/index.js
+++ graphql-server-example/index.js
@@ -1,11 +1,11 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import { ApolloServer, gql } from "apollo-server";
+
 const typeDefs = `
   type Author {
     id: Int!
     firstName: String
     lastName: String
-    """
-    the list of Posts by this author
-    """
     posts: [Post]
   }
 
@@ -16,7 +16,7 @@ const typeDefs = `
     votes: Int
   }
 
-  # the schema allows the following query:
+
   type Query {
     posts: [Post]
     author(id: Int!): Author
@@ -35,3 +35,29 @@ const posts = [
   { id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 },
   { id: 4, authorId: 3, title: 'Launchpad is Cool', votes: 7 },
 ];
+
+const resolvers = {
+  Query: {
+    posts: () => posts,
+    author: (_, { id }) => find(authors, { id }),
+  },
+
+  Author: {
+    posts: author => filter(posts, { authorId: author.id }),
+  },
+
+  Post: {
+    author: post => find(authors, { id: post.authorId }),
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const server = new ApolloServer({ schema });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
